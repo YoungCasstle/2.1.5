8ee8341071d1fcce7794ec2db9900c74b1ea1be3
YoungCasstle <kramer337@mail.ru>
2022-11-17 14:40:12 +0300

    2.1.5
    1

---
+++ src/main/java/koschei/models/Egg6.java
@@ -0,0 +1,13 @@
+package koschei.models;
+
+public class Egg6 {
+    private final Needle7 needle1;
+    public Egg6(Needle7 needle1){
+        this.needle1 = needle1;
+    }
+
+    @Override
+    public String toString() {
+        return ", в яйце иголка " + "";
+    }
+}
